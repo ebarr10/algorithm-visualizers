0bbb6cec58440634b547427c6289c8d8cf8ed5a2
Ethan <ej.barr00@gmail.com>
2026-01-25 23:32:52 -0500

    fixing deploy mistakes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     const isGhPages = mode === "gh-pages";
+
     return {
         plugins: [react()],
         base: isGhPages ? "/algorithm-visualizers/" : "/",
+        server: {
+            host: "127.0.0.1",
+            port: 5173,
+        },
     };
 });
